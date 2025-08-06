dc382ec70c1a8348da33d52408923a1596f42719
GuustavoPetry <ogustavopetry@gmail.com>
2025-08-06 11:58:48 -0300

    Removendo método AppDataSouce.initialize()

--- src/index.ts
+++ src/index.ts
@@ -7,34 +7,32 @@ import { AppDataSource } from "./database/data-source";
 require("dotenv").config();
 const cors = require("cors");
 
-AppDataSource.initialize()
-        .then(() => {
-            const app = express();
-            app.use(express.json());
 
-            const origins = ["https://localhost:4000"];
-            // Cors deve ser implementado antes de qualquer rota
-            app.use(cors({
-                origin: (origin:string, callback:Function) => {
-                    if(!origin) return callback(null, true);
+    const app = express();
+    app.use(express.json());
 
-                    if(origins.includes(origin)) {
-                        return callback(null, true);
-                    } else {
-                        return callback(new Error("Origem não permitida"));
-                    }
-                },
-                credentials: true,
-                methods: ["GET, POST, PUT, PATCH, DELETE"],
-                allowedHeader: ["Content-Type", "Authorization"]
-            }));
+    const origins = ["https://localhost:4000"];
+    // Cors deve ser implementado antes de qualquer rota
+    app.use(cors({
+        origin: (origin:string, callback:Function) => {
+            if(!origin) return callback(null, true);
 
-            app.use("/users", userRoutes);
-            app.use("/auth", authRoutes);
-            app.use("/brappi", brappiRoutes);
-            app.listen(process.env.API_PORT, () => {
-                console.log("Servidor Rodando na Porta :", process.env.API_PORT);
-                console.log("Banco de Dados Conectado na Porta :", process.env.DB_PORT);
-            });
-        })
-        .catch((error) => console.error("Conexão NÃO Estabelecida | ERRO: ", error));
+            if(origins.includes(origin)) {
+                return callback(null, true);
+            } else {
+                return callback(new Error("Origem não permitida"));
+            }
+        },
+        credentials: true,
+        methods: ["GET, POST, PUT, PATCH, DELETE"],
+        allowedHeader: ["Content-Type", "Authorization"]
+    }));
+
+    app.use("/users", userRoutes);
+    app.use("/auth", authRoutes);
+    app.use("/brappi", brappiRoutes);
+    app.listen(process.env.API_PORT, () => {
+        console.log("Servidor Rodando na Porta :", process.env.API_PORT);
+        console.log("Banco de Dados Conectado na Porta :", process.env.DB_PORT);
+    });
+        
