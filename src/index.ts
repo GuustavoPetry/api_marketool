cda268462210aa928a45f32d7cd567a3f0e06477
GuustavoPetry <ogustavopetry@gmail.com>
2025-08-06 11:47:16 -0300

    Configurando Deploy

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,30 @@ import authRoutes from "./routes/auth.routes";
 import brappiRoutes from "./routes/brappi.routes";
 import { AppDataSource } from "./database/data-source";
 require("dotenv").config();
+const cors = require("cors");
 
 AppDataSource.initialize()
         .then(() => {
             const app = express();
             app.use(express.json());
+
+            const origins = ["https://localhost:4000"];
+            // Cors deve ser implementado antes de qualquer rota
+            app.use(cors({
+                origin: (origin:string, callback:Function) => {
+                    if(!origin) return callback(null, true);
+
+                    if(origins.includes(origin)) {
+                        return callback(null, true);
+                    } else {
+                        return callback(new Error("Origem não permitida"));
+                    }
+                },
+                credentials: true,
+                methods: ["GET, POST, PUT, PATCH, DELETE"],
+                allowedHeader: ["Content-Type", "Authorization"]
+            }));
+
             app.use("/users", userRoutes);
             app.use("/auth", authRoutes);
             app.use("/brappi", brappiRoutes);
