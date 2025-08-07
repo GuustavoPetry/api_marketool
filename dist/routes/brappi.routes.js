24bd35f6c235ad8e517195a9f6ba8845c8cd1a32
GuustavoPetry <ogustavopetry@gmail.com>
2025-08-07 10:39:29 -0300

    Retirando arquivos .js do repositório

--- dist/routes/brappi.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const BrappiController_1 = require("../controllers/BrappiController");
-const routes = (0, express_1.Router)();
-routes.get("/assets", BrappiController_1.BrappiController.searchAssets);
-routes.get("/assets/:ticker", BrappiController_1.BrappiController.getOneAsset);
-exports.default = routes;
